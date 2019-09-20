59bb90b95c42ae5632a04390cf2fa9024854245f
Zev Hoffman <zevubu@gmail.com>
2019-09-20 13:15:22 -0700

    wrote out basic my friends array. Created basic home page layout.

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # FriendFinder
 Friend Finder - Node and Express Servers
+
+
+liquid Television!
+and few other shows I liked.
+
+Find your perfect match! 
+
+home.js
+area for the headed img.
+button to start questionnaire
+button to brows possible friends.
+
+liquid television icon 
+https://i.imgur.com/FePqJtH.jpg
+
+name: "Æon Flux"
+homeTown: The Nation of Monica
+Interests include: "acrobatics","Parkour","infiltration", "Anarchy", & "torture"
+
+
+name: "Trevor Goodchild",
+homeTown: "The country of Bregna",
+Interests include: "Power", "ambiguity", & "Science!"
+
+name: "The Maxx",
+homeTown: "The Outback",
+Interests include: "The sound the ISes make when I squish them!", "Protector of the Jungle Queen", & "Loose Change"
+
+
+name: "Sifle",
+homeTown: "I'll be right back.",
+Interests include:
+
+name: "Olly",
+homeTown: "Just over there.",
+Interests include: 
+
+
+Chester
+
+
+
---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+let Friends = [
+    {
+        name: "Æon Flux",
+        homeTown: "The Nation of Monica",
+        Interests:[ "acrobatics","Parkour","infiltration", "Anarchy", "torture"]
+
+    },{
+        name: "Trevor Goodchild",
+        homeTown: "The country of Bregna",
+        Interests: ["Power", "ambiguity","Science!"]
+    },{
+        name: "The Maxx",
+        homeTown: "The Outback",
+        Interests: ["The sound the ISes make when I squish them!", "Protector of the Jungle Queen","Loose Change"]
+
+    },{
+        name: "Sifle",
+        homeTown: "I'll be right back.",
+        Interests:[]
+    },{
+        name: "Olly",
+        homeTown: "Just over there.",
+        Interests: ["Singing the songs of life!"]
+    },{
+        name: "Chester",
+        homeTown: "Yeah...Aaaa...Yeah",
+        Interests: ["yeah","uuh cool", "umm Yeah","what? oooh","I think uuhhh yeah"]
+    },{
+        name: "Daria",
+        homeTown: "Lawndale",
+        Interests: ["yeah","uuh cool", "umm Yeah","what? oooh","I think uuhhh yeah"]
+    }
+]
+]
