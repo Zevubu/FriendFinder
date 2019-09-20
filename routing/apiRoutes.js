f6181f133d9b63af2b9d777ae97e6700a3f1048c
Zev Hoffman <zevubu@gmail.com>
2019-09-20 14:04:58 -0700

    wrote out a base model for my apiRoutes

---
+++ routing/apiRoutes.js
@@ -0,0 +1,12 @@
+const surveyData = require("../app/data/friends");
+
+module.exports = function(app){
+    app.get("/api/tables", function(req,res){
+        res.json(surveyData);
+    });
+
+    // posts all friends
+    app.post("/api/browse", function(req,res){
+        surveyData.push(req.body);
+    })
+}
--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,4 +1,16 @@
 const path = require("path");
 module.exports = function(app){
-    
-}
+    // connect to survey
+    app.get("./survey", function(req,res){
+        res.sendFile(path.join(_dirname, "../public/survey.html"));
+    });
+    // connect to browse
+    app.get("./browse", function(req,res){
+        res.sendFile(path.join(__dirname, "../public/browse.html"));
+    });
+    // go home
+    app.get("*", function(req,res){
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });   
+};
+
